994c0f877fdef78620d1768cfb27e87bb2c0b15f
SzakacsAdam <szakacs.adam.98@gmail.com>
2020-09-26 23:20:12 +0200

    first commit

---
+++ Python_Crash_Course/githubhelp.py
@@ -0,0 +1,13 @@
+#git init
+#ha új mappában vagyok
+
+#git add hello.py
+
+#git status
+#git rm-- cached hello.py
+#git add *.py
+#git commit -m "Changes happend"
+#git remote add origin git@github.com:SzakacsAdam/Hello.git
+#git push -u origin master
+
+
